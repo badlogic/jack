200918a2ed42c45bc28ed02df6fb886b63098ba0
badlogic <contact@badlogicgames.com>
2012-08-24 16:26:39 +0200

    gah

--- src/com/badlogic/jack/JavaSourceProvider.java
+++ src/com/badlogic/jack/JavaSourceProvider.java
@@ -1,5 +1,12 @@
 package com.badlogic.jack;
 
+/**
+ * Reads all .java files in a directory and it's subdirectories,
+ * splits them up into lines and allows a client to retrieve
+ * individual lines
+ * @author mzechner
+ *
+ */
 public class JavaSourceProvider {
 
 }
